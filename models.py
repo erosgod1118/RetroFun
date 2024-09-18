7f3850391cdb73a73b52b07e5174ec81ec740a0b
Eros God <erosgod19921118@gmail.com>
2024-09-18 18:58:31 -0400

    Add import_orders.py file.

--- models.py
+++ models.py
@@ -1,10 +1,14 @@
 from sqlalchemy import String, ForeignKey, Table, Column
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship, WriteOnlyMapped
+
 from db import Model
-from typing import List
+
+from typing import Optional
+from uuid import UUID, uuid4
+from datetime import datetime
 
 ProductCountry = Table(
-    'products_country',
+    'products_countries',
     Model.metadata,
     Column('product_id', ForeignKey('products.id'), primary_key=True, nullable=False),
     Column('country_id', ForeignKey('countries.id'), primary_key=True, nullable=False)
@@ -17,9 +21,10 @@ class Product(Model):
     name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
     manufacturer_id: Mapped[int] = mapped_column(ForeignKey("manufacturers.id"), index=True)
     year: Mapped[int] = mapped_column(index=True)
-    cpu: Mapped[str] = mapped_column(String(32))
+    cpu: Mapped[Optional[str]] = mapped_column(String(32))
     manufacturer: Mapped['Manufacturer'] = relationship(back_populates='products')
-    countries: Mapped[List['Country']] = relationship(secondary=ProductCountry, back_populates='products')
+    countries: Mapped[list['Country']] = relationship(secondary=ProductCountry, back_populates='products')
+    order_items: WriteOnlyMapped['OrderItem'] = relationship(back_populates='product')
 
     def __repr__(self):
         return f"Product({self.id}, {self.name})"
@@ -29,7 +34,7 @@ class Manufacturer(Model):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
-    products: Mapped[List['Product']] = relationship(back_populates='manufacturer')
+    products: Mapped[list['Product']] = relationship(back_populates='manufacturer')
 
     def __repr__(self):
         return f"Manufacturer({self.id}, {self.name})"
@@ -39,7 +44,41 @@ class Country(Model):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(32), index=True, unique=True)
-    products: Mapped[List['Product']] = relationship(secondary=ProductCountry, back_populates='countries')
+    products: Mapped[list['Product']] = relationship(secondary=ProductCountry, back_populates='countries')
 
     def __repr__(self):
         return f'Country({self.id}, {self.name})'
+    
+class Order(Model):
+    __tablename__ = 'orders'
+
+    id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
+    timestamp: Mapped[datetime] = mapped_column(default=datetime.utcnow, index=True)
+    customer_id: Mapped[UUID] = mapped_column(ForeignKey('customers.id'), index=True)
+    customer: Mapped['Customer'] = relationship(back_populates='orders')
+    order_items: Mapped[list['OrderItem']] = relationship(back_populates='order')
+
+    def __repr__(self):
+        return f'Order({self.id.hex})'
+
+class Customer(Model):
+    __tablename__ = 'customers'
+
+    id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
+    name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
+    address: Mapped[Optional[str]] = mapped_column(String(128))
+    phone: Mapped[Optional[str]] = mapped_column(String(32))
+    orders: WriteOnlyMapped[list['Order']] = relationship(back_populates='customer')
+
+    def __repr__(self):
+        return f'Customer({self.id.hex}, "{self.name})'
+    
+class OrderItem(Model):
+    __tablename__ = 'orders_items'
+
+    product_id: Mapped[int] = mapped_column(ForeignKey('products.id'), primary_key=True)
+    order_id: Mapped[UUID] = mapped_column(ForeignKey('orders.id'), primary_key=True)
+    unit_price: Mapped[float]
+    quantity: Mapped[int]
+    product: Mapped['Product'] = relationship(back_populates='order_items')
+    order: Mapped['Order'] = relationship(back_populates='order_items')
