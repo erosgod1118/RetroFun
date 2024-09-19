3e09c9fd93977ba98f575cd59102c85725bab894
Eros God <erosgod19921118@gmail.com>
2024-09-19 07:20:11 -0400

    Implement import_views.py & import_articles.py files.

--- models.py
+++ models.py
@@ -26,6 +26,7 @@ class Product(Model):
     countries: Mapped[list['Country']] = relationship(secondary=ProductCountry, back_populates='products')
     order_items: WriteOnlyMapped['OrderItem'] = relationship(back_populates='product')
     reviews: WriteOnlyMapped['ProductReview'] = relationship(back_populates='product')
+    blog_articles: WriteOnlyMapped['BlogArticle'] = relationship(back_populates='product')
 
     def __repr__(self):
         return f"Product({self.id}, {self.name})"
@@ -71,6 +72,7 @@ class Customer(Model):
     phone: Mapped[Optional[str]] = mapped_column(String(32))
     orders: WriteOnlyMapped[list['Order']] = relationship(back_populates='customer')
     product_reviews: WriteOnlyMapped['ProductReview'] = relationship(back_populates='customer')
+    blog_users: WriteOnlyMapped[list['BlogUser']] = relationship(back_populates='customer')
 
     def __repr__(self):
         return f'Customer({self.id.hex}, "{self.name})'
@@ -95,3 +97,60 @@ class ProductReview(Model):
     comment: Mapped[Optional[str]] = mapped_column(Text)
     product: Mapped['Product'] = relationship(back_populates='reviews')
     customer: Mapped['Customer'] = relationship(back_populates='product_reviews')
+
+class BlogArticle(Model):
+    __tablename__ = 'blog_articles'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(128), index=True)
+    author_id: Mapped[int] = mapped_column(ForeignKey('blog_authors.id'), index=True)
+    product_id: Mapped[Optional[int]] = mapped_column(ForeignKey('products.id'), index=True)
+    timestamp: Mapped[datetime] = mapped_column(default=datetime.utcnow, index=True)
+    author: Mapped['BlogAuthor'] = relationship(back_populates='articles')
+    product: Mapped[Optional['Product']] = relationship(back_populates='blog_articles')
+    views: WriteOnlyMapped[list['BlogView']] = relationship(back_populates='article')
+
+    def __repr__(self):
+        return f'BlogArticle({self.id}, "{self.title}")'
+    
+class BlogAuthor(Model):
+    __tablename__ = "blog_authors"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(64), index=True)
+    articles: WriteOnlyMapped['BlogArticle'] = relationship(back_populates='author')
+
+    def __repr__(self):
+        return f'BlogAuthor({self.id}, "{self.name}")'
+    
+class BlogUser(Model):
+    __tablename__ = "blog_users"
+
+    id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
+    customer_id: Mapped[Optional[UUID]] = mapped_column(ForeignKey('customers.id'), index=True)
+    customer: Mapped[Optional['Customer']] = relationship(back_populates='blog_users')
+    sessions: WriteOnlyMapped[list['BlogSession']] = relationship(back_populates='user')
+
+    def __repr__(self):
+        return f'BlogUser({self.id.hex})'
+    
+class BlogSession(Model):
+    __tablename__ = "blog_sessions"
+
+    id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
+    user_id: Mapped[UUID] = mapped_column(ForeignKey('blog_users.id'), index=True)
+    user: Mapped['BlogUser'] = relationship(back_populates='sessions')
+    views: WriteOnlyMapped[list['BlogView']] = relationship(back_populates='session')
+
+    def __repr__(self):
+        return f'BlogSession({self.id.hex})'
+    
+class BlogView(Model):
+    __tablename__ = "blog_views"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    article_id: Mapped[int] = mapped_column(ForeignKey('blog_articles.id'))
+    session_id: Mapped[UUID] = mapped_column(ForeignKey('blog_sessions.id'))
+    timestamp: Mapped[datetime] = mapped_column(default=datetime.utcnow, index=True)
+    article: Mapped['BlogArticle'] = relationship(back_populates='views')
+    session: Mapped['BlogSession'] = relationship(back_populates='views')
